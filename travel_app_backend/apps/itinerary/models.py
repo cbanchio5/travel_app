15fc467aefbab87e20f8f0759428273277812dcf
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-07-30 10:46:31 +0200

    created itinerary models

--- travel_app_backend/apps/itinerary/models.py
+++ travel_app_backend/apps/itinerary/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+# apps/itinerary/models.py
+
+class Itinerary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='itineraries'
+    )
+    title = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title} ({self.start_date} to {self.end_date})"
+
+class ItineraryOption(models.Model):
+    itinerary = models.ForeignKey(Itinerary, on_delete=models.CASCADE, related_name='options')
+    description = models.JSONField(blank=True, null=True)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Option for {self.itinerary.title} at {self.location}"
