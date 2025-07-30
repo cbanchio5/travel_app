225002661d13e53be33f15e51e23b8f803560d44
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-07-30 15:01:23 +0200

    change response

--- travel_app_backend/apps/itinerary/services/itinerary_generator.py
+++ travel_app_backend/apps/itinerary/services/itinerary_generator.py
@@ -1,6 +1,6 @@
 import os
 import json
-from openai import OpenAI
+from openai import OpenAI, OpenAIError
 from dotenv import load_dotenv
 from apps.itinerary.models.itinerary_schema import ItineraryAlternative
 
@@ -15,21 +15,26 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 def generate_itinerary_options(destination, arrival, departure):
     prompt_text = f"destination: {destination}, arrival: {arrival}, departure: {departure}"
     
-    response = client.responses.parse(
-        model="gpt-4o-mini",
-        input=[
+    try:
+        response = client.responses.parse(
+         model="gpt-4o-mini",
+            input=[
             {"role": "system", "content": "You are a helpful travel assistant. Create 3 alternative itineraries for the destination. They should all include what to do and see on each day. Structure should be a dictionary of alternatives, each alternative being a dictionary of their own containing what to do on each day"},
-        {
+            {
             "role": "user",
             "content": prompt_text,
-        },
-    ],
-        text_format=ItineraryAlternative,
-    )
+            },
+            ],
+            text_format=ItineraryAlternative,
+            )
+    except OpenAIError as e:
+        raise RuntimeError(f"OpenAI API error: {e}")
+    except Exception as e:
+        raise RuntimeError(f"Unexpected error: {e}")
 
     print(response.output[0].content[0].parsed)
     print(type(response))
 
     # Assuming response is JSON string:
     
-    return "options_data"  # list of dicts with description & location
+    return response.output[0].content[0].parsed  # list of dicts with description & location
