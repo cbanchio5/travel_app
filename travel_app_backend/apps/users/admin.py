5d0d6cff904d30320434f3f28bde585630ec86d4
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-07-30 10:28:31 +0200

    added some ther properties on user model'

--- travel_app_backend/apps/users/admin.py
+++ travel_app_backend/apps/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 
 # Register your models here.
+# apps/users/admin.py
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {
+            'fields': ('avatar', 'phone_number', 'date_of_birth', 'country')
+        }),
+    )
+
--- travel_app_backend/apps/users/models.py
+++ travel_app_backend/apps/users/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add any additional fields you want here
     phone_number = models.CharField(max_length=20, blank=True, null=True)
-    # For example, add a profile picture
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.username
