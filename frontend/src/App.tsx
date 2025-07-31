758d03f74c7d30f2a102d7adbc6d8ba3b99032bf
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-07-31 11:53:02 +0200

    created frontend react template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <button className="outline outline-2 outline-offset-2 ...">Button A</button>
+    </h1>
+  );
+}
+
+export default App;
--- travel_app_backend/apps/itinerary/models.py
+++ travel_app_backend/apps/itinerary/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-# apps/itinerary/models.py
 
 class Itinerary(models.Model):
     user = models.ForeignKey(
