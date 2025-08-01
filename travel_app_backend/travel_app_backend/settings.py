cc449a1ebcaac08ecc5824577b14c0b1f1b36195
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-01 11:30:17 +0200

    dockerized app

--- travel_app_backend/travel_app_backend/settings.py
+++ travel_app_backend/travel_app_backend/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Adjust if you use different host/port
+CELERY_BROKER_URL = 'redis://redis:6379/0'  # Adjust if you use different host/port
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
