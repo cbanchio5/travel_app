9199b92deb3605eac69c7b8e1197c94d3c152aac
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-04 11:26:53 +0200

    added print statement2

--- travel_app_backend/travel_app_backend/celery.py
+++ travel_app_backend/travel_app_backend/celery.py
@@ -9,7 +9,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 #app.conf.broker_url = 'redis://redis:6379/0/0'
 #app.conf.result_backend = 'redis://redis:6379/0'
-app.conf.broker_url = os.environ.get('REDIS_URL_PROD', 'redis://redis:6379/0/0')
-app.conf.result_backend = os.environ.get('REDIS_URL_PROD', 'redis://redis:6379/0')
+app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://redis:6379/0/0')
+app.conf.result_backend = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
 
 app.autodiscover_tasks()
